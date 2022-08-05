fb334269faa30e7b670dff1d6d4796f32a56a880
Lu <3091907359@qq.com>
2022-08-05 13:47:20 +0800

    first commit

---
+++ idea/git-test/src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author: Collin
+ * @Date: 2022/08/05 13:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
